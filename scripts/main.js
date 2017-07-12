47246352177b587040039bf4f4e91d4afc844d97
Tom Cornell <tom.cornell@gmail.com>
2017-07-12 11:45:32 -0400

    First stab at styling
    Just center everything on the front page.

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,20 @@ window.onload = function () {
 var CCCircus = (function () {
 
 function main() {
+	init();
+}
 
+function init() {
+	document.getElementById("startGame").onclick = doStartGame;
+	document.getElementById("exitGame").onclick = doExitGame;
+}
+
+function doStartGame() {
+	alert("doStartGame");
+}
+
+function doExitGame() {
+	alert("doExitGame");
 }
 
 return {
